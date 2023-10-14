ad2681df9ab7367411aabf68769c2de26b3223e9
Devashish08 <devashish.cs025@gmail.com>
2023-10-14 23:24:50 +0530

    First Commit

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const express = require("express");
+const { register, login } = require("../controllers/auth");
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
